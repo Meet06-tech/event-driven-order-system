016f035d836659cda51831a0a8a2cf1c35b12cd5
Meet <meetbhoi05@gmail.com>
2026-03-04 15:24:36 +0530

    Initial microservices architecture

---
+++ order-service/src/main/java/com/meet/order_service/controller/api.java
@@ -0,0 +1,13 @@
+package com.meet.order_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class api {
+
+    @GetMapping
+    public String print(){
+        return "Hello World";
+    }
+}

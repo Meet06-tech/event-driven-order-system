c3df9e782bc9ea4ebd56cd08a8a3f51aeae7ddda
Meet <meetbhoi05@gmail.com>
2026-03-06 21:52:20 +0530

    order service done

---
+++ order-service/src/main/java/com/meet/order_service/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.meet.order_service.service;
+
+import com.meet.order_service.model.Order;
+import com.meet.order_service.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Order order){
+        order.setStatus("CREATED");
+        return orderRepository.save(order);
+    }
+}
